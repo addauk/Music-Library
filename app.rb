ee0c3a6c352c7f9e3063bdd43724c4f33e8c84cd
addauk <addauk@gmail.com>
2023-01-31 14:09:52 +0000

    Up to first exercise of music library

---
+++ app.rb
@@ -0,0 +1,13 @@
+# file: app.rb
+
+require_relative 'lib/database_connection'
+require_relative 'lib/artist_repository'
+
+# We need to give the database name to the method `connect`.
+DatabaseConnection.connect('music_library')
+
+artist_repository = ArtistRepository.new
+
+artist_repository.all.each do |record|
+  p record
+end
